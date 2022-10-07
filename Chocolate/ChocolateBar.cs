3f00c6ce36add974ae3d657fc6028bdc967d04b2
Luke Barkess <luke.barkess@icloud.com>
2022-10-07 15:19:43 +0100

    rolo

---
+++ Chocolate/ChocolateBar.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chocolate
+{
+    class ChocolateBar
+    {
+        public string name;
+
+        /// <summary>
+        /// Number of calories per chocolate bar
+        /// </summary>
+        public int calories;
+
+        /// <summary>
+        /// Weight of the chocolate bar in grams
+        /// </summary>
+        public double weightInGrams;
+
+        public ChocolateBar(string name, int calories, double weightInGrams)
+        {
+            this.name = name;
+            this.calories = calories;
+            this.weightInGrams = weightInGrams;
+        }
+
+        /// <summary>
+        /// Eat the chocolate bar
+        /// </summary>
+        public void Eat()
+        {
+            Console.WriteLine($"Yum you ate a {name} which was {calories} calories");
+            weightInGrams = 0;
+        }
+
+        public void Throw()
+        {
+            Console.WriteLine("Ouch");
+        }
+    }
+}
--- Chocolate/Program.cs
+++ Chocolate/Program.cs
@@ -10,6 +10,13 @@ namespace Chocolate
     {
         static void Main(string[] args)
         {
+            ChocolateBar boost = new ChocolateBar("Boost", 163, 31.5);
+            ChocolateBar rolo = new ChocolateBar("Rolo", 200, 41.6);
+
+            boost.Eat();
+            rolo.Eat();
+
+            Console.ReadLine();
         }
     }
 }
